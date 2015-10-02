a3aa8654d05443d7145b661a9352fa9361672379
Sam Martinez <sammartinez19@gmail.com>
2015-10-02 15:25:14 -0700

    able to use directive to change the night mode text red

--- directives/Invert.js
+++ directives/Invert.js
@@ -1,7 +1,7 @@
 nickFlicks.directive("invert", function() {
-  return function(scope, element) {
+  return function(scope, element, attrs) {
     element.bind("click", function() {
-      element.toggleClass("bg-danger");
+      element.toggleClass(attrs.invert);
     });
   }
 });
