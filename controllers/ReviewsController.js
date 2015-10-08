8d5fa58720042bd0c924429bf277829f4aa672ba
Sam Martinez <sammartinez19@gmail.com>
2015-10-07 22:38:03 -0700

    add comment

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -2,5 +2,6 @@ nickFlicks.controller('ReviewsCtrl',  function ReviewsCtrl($scope, $stateParams,
   $scope.reviews = ReviewsFactory.reviews;
   $scope.ReviewsFactory = ReviewsFactory;
 
+  //Finds the current review and displays it
   $scope.currentReview = UtilitiesFactory.findById(ReviewsFactory.reviews, $stateParams.reviewId);
 });
